20310759ed39f6e39923fe058fdf9f307b1d3824
stas <avdosevstas@mail.ru>
2017-06-21 11:51:05 +0300

    test

--- bash_parser_o.py
+++ bash_parser_o.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 if __name__ == '__main__':
+    url_page_1 ="http://irr.ru/real-estate/"
+    r_1 = requests.get(url_page_1)
+    soup_1 = BeautifulSoup(r_1.text, 'html.parser')
+    #for item1_1 in soup_1.find_all('a href', 'mainPageBlockCategory__subTitle'):
+        for item2_1 in item1_1.find_all('a href', 'mainPageBlockCategory__subTitle'):
+            tek_page=item2_1.text
     url_page = 'http://krasnodar.irr.ru/real-estate/apartments-sale/new/studiya-im-evgenii-zhigulenko-ul-9-advert630802162.html'
     r = requests.get(url_page)
     soup = BeautifulSoup(r.text, 'html.parser')
