21adeb16a360cf173617d614bc9cf7b3fde4ea0d
stas <avdosevstas@mail.ru>
2017-06-21 10:35:07 +0300

    + try

--- bash_parser.py
+++ bash_parser.py
@@ -5,23 +5,26 @@ if __name__ == '__main__':
     url_page = 'http://krasnodar.irr.ru/real-estate/apartments-sale/new/studiya-im-evgenii-zhigulenko-ul-9-advert630802162.html'
     r = requests.get(url_page)
     soup = BeautifulSoup(r.text, 'html.parser')
-    
-    header = soup.find('h1', 'productPage__title').text
-    print(header)
-    
-    cena=soup.find('div', 'productPage__price').text
-    print(cena)
-    
-    opisanie_k=soup.find('p', 'productPage__descriptionText').text
-    print(opisanie_k)
+    try:
+        header = soup.find('h1', 'productPage__title').text
+        print(header)
+    except:
+        print("")
+    try:
+        cena=soup.find('div', 'productPage__price').text
+        print(cena)
+    except:
+        print("")
     for item in soup.find_all('span', 'productPage__characteristicsItemValue'):
         pod_header = item.text
         print(pod_header)
-
-    #odin_is_3_div_info=soup.find_all('div', 'productPage__infoColumnBlock')
-        s4et=1
+    try:    
+        opisanie_k=soup.find('p', 'productPage__descriptionText').text
+        print(opisanie_k)
+    except:
+        print("")
+    s4et=1
     for item in soup.find_all('div', 'productPage__infoColumnBlock'):
-        #print("info"+s4et+""+item1.text)
         print('info{}'.format(s4et))
         for item1 in item.find_all('li', 'productPage__infoColumnBlockText'):
             print(item1.text)
