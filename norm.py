a7cfb4feb6c97b2bf0755a8c119042a0f81b118a
stas <avdosevstas@mail.ru>
2017-06-24 15:52:16 +0300

    мб без ссылки без картинок

--- norm.py
+++ norm.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     url_page_1 ="http://irr.ru/real-estate/"
     r_1 = requests.get(url_page_1)
     soup_1 = BeautifulSoup(r_1.text, 'html.parser')
+    #страница со всеми разделами
     for item1_1_bloki in soup_1.find_all('div', 'mainPageBlockCategory__wrapper'):
         odin_div_a="http://irr.ru"+item1_1_bloki.find_all("a")[0].attrs["href"]
         url_page_2 =odin_div_a
@@ -23,6 +24,7 @@ if __name__ == '__main__':
         print(koli4_str.text)
         i1=int(koli4_str.text)
         i=0
+        #по страницам снизу
         while i < i1:
             i+=1
             print(i)
@@ -30,12 +32,14 @@ if __name__ == '__main__':
             print(odin_url_nomer_str)
             r_123 = requests.get(odin_url_nomer_str)
             soup_123 = BeautifulSoup(r_123.text, 'html.parser')
+            #по списку с КВ
             for item2_1 in soup_123.find_all('div', 'listing__itemTitleWrapper'):
                 for a_elm in item2_1.find_all("a"):  
                     url_page = a_elm.attrs["href"]
                     print(url_page)
                     r = requests.get(url_page)
                     soup = BeautifulSoup(r.text, 'html.parser')
+                    #СТРАНИЦА
                     try:
                         header = soup.find('h1', 'productPage__title').text
                         header=re.sub('\s+',' ',header)
@@ -50,6 +54,23 @@ if __name__ == '__main__':
                         print(cena)
                     except:
                         print("")
+                    data['nomer']=re.sub('\s+',' ',soup.find('div', 'productPage__phoneText').text)
+                    #productPage__infoTextBold
+                    tt1=soup.find('div', 'productPage__inlineWrapper')
+                    data['name']=re.sub('\s+',' ',tt1.find('div', 'productPage__infoTextBold').text)
+
+                    for item in soup.find_all('div', 'productPage__infoItem'):
+                        tt2=re.sub('\s+','',item.text)
+                        #print("1========================1")
+                        #print(tt2)
+                        #time.sleep(5)
+                        if tt2.find("Место:")!=-1:
+                            data["mesto"]=re.sub('\s+',' ',item.find('div', 'productPage__infoTextBold').text)
+                            
+                        if tt2=="":
+                            
+                            data["ssilka"]=re.sub('\s+',' ',item.find('a').attrs["href"])
+
                     for item in soup.find_all('div', 'productPage__characteristicsItem'):
                         i_temp1=item.find('span', 'productPage__characteristicsItemValue')
                         i_temp1=re.sub('\s+',' ',i_temp1.text)
