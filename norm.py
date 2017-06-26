16edec9e1651e4aa072bb512cc19cc91afe6b6a8
stas <avdosevstas@mail.ru>
2017-06-26 15:24:30 +0300

    + -g

--- norm.py
+++ norm.py
@@ -4,11 +4,8 @@ import time
 import json
 import re
 
-if __name__ == '__main__':
-    data={}
-    data_error={}
+def go_play():
     koli4estvo_zapisey=0
-    koli4estvo_error=0 
     url_page_1 ="http://russia.irr.ru/real-estate/"
     r_1 = requests.get(url_page_1)
     soup_1 = BeautifulSoup(r_1.text, 'html.parser')
@@ -29,7 +26,6 @@ if __name__ == '__main__':
         url_page_2 =odin_div_a
         r_2 = requests.get(url_page_2)
         soup_2 = BeautifulSoup(r_2.text, 'html.parser')
-
         koli4_str1=soup_2.find_all('li', 'pagination__pagesItem')
         koli4_str=koli4_str1[len(koli4_str1)-1]
         last_url=koli4_str.find("a").attrs["href"]
@@ -49,6 +45,9 @@ if __name__ == '__main__':
                     r = requests.get(url_page)
                     soup = BeautifulSoup(r.text, 'html.parser')
                     #СТРАНИЦА
+                    data_error={}
+                    data={}
+                    koli4estvo_error=0 
                     data["тип_жилья"]=tip_gilya_str
                     data["тип_объявления"]=tip_obyavleniya
                     data["url_page"]=url_page
@@ -145,9 +144,6 @@ if __name__ == '__main__':
                             print("")
                         s4et+=1
                     try:
-                        with open('data.json', 'a', encoding='utf-8') as fh:
-                            fh.write(json.dumps(data, ensure_ascii=False))
-                        data={}
                         koli4estvo_zapisey+=1
                         print(koli4estvo_zapisey)
                         print("ERROR="+str(koli4estvo_error))
@@ -155,12 +151,23 @@ if __name__ == '__main__':
                             data_error["url"]=url_page
                             with open('data_error.json', 'a', encoding='utf-8') as fh:
                                 fh.write(json.dumps(data_error, ensure_ascii=False))
-                            
-                            koli4estvo_error=0
-                        data_error={}
+                        yield data
                     except:
                         print("===================ERROR========================")
                         data_error[str(koli4estvo_zapisey)+":"+"save"]="save"
                         time.sleep(10)
                     
                     print("______________________")
+
+
+if __name__ == '__main__':
+    data=go_play()
+    for i in data:
+        with open('data.json', 'a', encoding='utf-8') as fh:
+            fh.write(json.dumps(i, ensure_ascii=False))
+        data={}
+                            
+        
+        
+    
+    
